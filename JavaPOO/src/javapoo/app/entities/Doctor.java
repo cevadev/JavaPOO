cb66912e7233a1357e1568be795c5ee4f120c31b
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2020-02-07 09:53:42 -0300

    Java POO

---
+++ JavaPOO/src/javapoo/app/entities/Doctor.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapoo.app.entities;
+
+/**
+ *
+ * @author PC
+ */
+public class Doctor {
+    int id;
+    String name;
+    String speciality;
+    
+    public void showName()
+    {
+    }
+}
---
+++ JavaPOO/src/javapoo/client/app/Client.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapoo.client.app;
+
+/**
+ *
+ * @author PC
+ */
+public class Client {
+    public static void main(String[] args) {
+        
+    }
+}
